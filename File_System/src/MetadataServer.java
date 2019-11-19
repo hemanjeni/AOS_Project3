92465fdf225395d9d50a86c465feaeb0f5593769
hemanjeni <44955179+hemanjeni@users.noreply.github.com>
2019-11-19 12:03:09 -0600

    Metadata server initialiazation

--- File_System/src/MetadataServer.java
+++ File_System/src/MetadataServer.java
@@ -21,6 +21,10 @@ public class MetadataServer {
 
 	public MetadataServer(String args[]) throws IOException {
 
+		
+		ConfigProperties prop = new ConfigProperties();
+		int metadataServeraddressforserver1 =  Integer.parseInt(prop.getPropValues("metadataServeraddressforserver1"));
+		
 		try {
 			//logger for file server
 			File logDir = new File("./logs/");
@@ -33,7 +37,20 @@ public class MetadataServer {
 			// fh.setLevel(logLevel);
 			logger.addHandler(fh);
 			logger.info("MetadataServer  initialization");
+			
+			
+			for (int i = 0; i < 7; i++) 
+			{
+			ss[i] = new ServerSocket(metadataServeraddressforserver1 + i);
+			s[i] = ss[i].accept();
+			dos[i] = new DataOutputStream(s[i].getOutputStream());
+			dis[i] = new DataInputStream(s[i].getInputStream());
 
+			t[i] = new Thread(new ChannelHandler(s[i]));
+			t[i].start();
+			System.out.print("Starting thread number" + i);
+			logger.info("Starting thread number" + i);
+			}
 			// accept connections from clients
 		}
 		catch (IOException e) {
@@ -41,7 +58,34 @@ public class MetadataServer {
 			e.printStackTrace();
 		}
 		}
+	class ChannelHandler implements Runnable {
+		DataInputStream datainput;
+		DataOutputStream dataoutput;
+		Socket socket;
 
+		public ChannelHandler(Socket s) {
+			try {
+				// initializing data i/p and o/p streams
+				datainput = new DataInputStream(s.getInputStream());
+				dataoutput = new DataOutputStream(s.getOutputStream());
+				System.out.print("after socket initialization" + datainput + " " + dataoutput + " " + s);
+				logger.info("After socket initialization" + datainput + " " + dataoutput + " " + s);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		public void run() {
+			try {
+
+				
+				}
+			 catch (Exception ex) {
+				ex.printStackTrace();
+			}
+
+		}
+	}
 	/* server number to be given as argument while running */
 	public static void main(String[] args) {
 		try {

4e8f64e681cb378d2d7711d42ccabbe42b3c6b5d
hemanjeni <44955179+hemanjeni@users.noreply.github.com>
2019-11-30 13:25:55 -0600

    Update createFile.java

--- File_System/src/createFile.java
+++ File_System/src/createFile.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -11,8 +12,8 @@ public class createFile {
 		try {
 			f = new RandomAccessFile("./files/"+filename, "rw");
 			f.setLength(4096*1024);//4096*1024
-			long l = f.getFilePointer();
-			System.out.println("current offset"+l);
+		//	long l = f.getFilePointer();
+		//	System.out.println("current offset"+l);
 		    bytes = f.length();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -24,10 +25,11 @@ public class createFile {
 		System.out.println("file "+filename+"created with file size:"+megabytes);
 		
 	}
-	public static void filewithString(String filename,String filechars) throws IOException
+	public static int filewithString(String filename,String filechars) throws IOException
 	{
 		RandomAccessFile f;
 		double bytes = 0;
+		int pointer = 0;
 		try {
 			f = new RandomAccessFile("./files/"+filename, "rw");
 			f.setLength(4096*1024);//4096*1024
@@ -38,53 +40,57 @@ public class createFile {
 	        f.writeBytes(filechars);
 		    System.out.println("offset after write"+f.getFilePointer());
 		    long l2 = f.getFilePointer();
+		     pointer =(int)l2;
 			System.out.println("current offset after write"+l2);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		
+		return pointer;
 	}
-	public static boolean fileAppendString(String filename,String filechars) throws IOException
+	public static int fileAppendString(String filename,String filechars,int offset) throws IOException
 	{
-		boolean result=false; 
+		//boolean result=false; 
 		RandomAccessFile f;
 		double bytes = 0;
+		int pointer=0;
 		try {
 			RandomAccessFile raf = new RandomAccessFile("./files/"+filename, "rw");
-			raf.seek(raf.length());
+			raf.seek(offset);
 		//	int leftspace = 4096 -(int)raf.length();
 			if(!filechars.equals("NULL"))
 			{
 			raf.write(filechars.getBytes());
 			System.out.println("append op done pointer at"+raf.getFilePointer());
-			result=true;
+			 pointer = (int)raf.getFilePointer();
+			//result=true;
 			}
 			
 			else
 			{
 				
 				raf.write(null);
-				result=false;
+				pointer=offset;
+				
 			}
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return result;	
+		return pointer;	
 		
 	}
 	
-	public static boolean checkfileAppend(String filename,int lengthofappend) throws IOException
+	public static boolean checkfileAppend(String filename,int lengthofappend, int fileoffset) throws IOException
 	{
 		boolean result=false; 
 		RandomAccessFile f;
 		double bytes = 0;
 		try {
 			RandomAccessFile raf = new RandomAccessFile("./files/"+filename, "rw");
-			int remaining = 4096-(int)raf.length();
+			int remaining = 4096-(fileoffset+1);
 			if(remaining >lengthofappend)
 			{
 				result=true;
@@ -103,5 +109,48 @@ public class createFile {
 		return result;	
 		
 	}
+	public static int renameFile(String filename,String newfile) throws IOException
+	{
+		
+		boolean result=true;
+		int pointer=0;
+		try{
+	        RandomAccessFile data = new RandomAccessFile("./files/"+filename,"rw");
+	        RandomAccessFile datacopy = new RandomAccessFile("./files/"+newfile,"rw");
+
+	        datacopy.seek(0);
+	        for(int i = 0; i < data.length(); i++){
+	            datacopy.write(data.read());
+	            
+	        }
+	        long l2 = data.getFilePointer();
+		     pointer =(int)l2;
+	        data.close();
+	        datacopy.close();
+	    }catch(IOException e){
+	    	result=false;
+	    }
+		
+		
+		File file = new File("./files/"+filename);
+
+		//Use the try-with-resources to create the RandomAccessFile
+		//Which takes care of closing the file once leaving the try block
+		try(RandomAccessFile randomFile = new RandomAccessFile(file, "rw")){
+
+		    //do some writing to the file...
+			
+			randomFile.close();
+			file.delete();
+		}
+		catch(Exception ex){
+			result=false;
+		    ex.printStackTrace();
+		}
+		
+		
+		return pointer;
+	}
+	
 	
 }
